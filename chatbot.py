75203e77f123e994a87105f6764a2196350c81b2
Chatbot <134295092+ChatbotMedias24@users.noreply.github.com>
2023-12-15 15:12:15 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -141,7 +141,7 @@ def main():
         if query:
             docs = VectorStore.similarity_search(query=query, k=3)
  
-            llm = llm = ChatOpenAI(model_name="gpt-4-1106-preview")
+            llm = ChatOpenAI(model_name="gpt-4-1106-preview")
             chain = load_qa_chain(llm=llm, chain_type="stuff")
             with get_openai_callback() as cb:
                 response = chain.run(input_documents=docs, question=query)
